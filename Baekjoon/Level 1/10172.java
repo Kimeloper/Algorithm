1bbd4bc3e278c66f28bb752d5fbfd985a997e73f
Kimeloper <sesame.kim321@gmail.com>
2024-11-30 02:45:08 +0900

    Create 10172 개.java
    [Bronze V]
    
    ", `, \ 등의 문자에 주의하며 개를 출력하는 문제🐕
    https://www.acmicpc.net/problem/10172

---
+++ Baekjoon/Level 1/10172.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
+
+/*
+    이스케이프 문자(Escape sequence)
+    
+     입력           출력
+      \\    ->     \
+      \"    ->     "
+      \'    ->     '
+      \?    ->     ?
+      \n    ->     개행
+      \t    ->     탭
+      \r    ->     캐리지 리턴
+      \b    ->     백스페이스
+
+      '은 "" 안에서는 \없이 사용할 수 있다.
+      " 또한 ''안에서는 \없이 사용할 수 있다.
+ */
