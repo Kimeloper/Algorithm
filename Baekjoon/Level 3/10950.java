ed7e0b979afb4ffbf9f37143791603a9e88395ba
Kimeloper <sesame.kim321@gmail.com>
2024-12-03 19:32:38 +0900

    Create 10950 A+B - 3.java
    [Bronze V]
    
    A+B를 여러 번 출력하는 문제
    https://www.acmicpc.net/problem/10950

---
+++ Baekjoon/Level 3/10950.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();//테스트 개수
+
+        //테스트 개수만큼 반복
+        for(int i = 0 ; i < T ; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            System.out.println(A + B);
+        }
+
+        sc.close();
+
+    }
+}
