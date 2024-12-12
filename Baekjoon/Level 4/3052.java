13ce140b6b08284356833e4832ad22066604bc60
Kimeloper <sesame.kim321@gmail.com>
2024-12-12 23:37:36 +0900

    Create 3052 나머지.java
    [Bronze II]
    
    배열을 활용하여 서로 다른 값의 개수를 찾는 문제
    https://www.acmicpc.net/problem/3052

---
+++ Baekjoon/Level 4/3052.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[10];
+        int count = 1 ;
+
+        for(int i = 0 ; i < 10 ; i++){
+            arr[i] = sc.nextInt() % 42;
+        }
+
+        Arrays.sort(arr);
+        for(int i = 0 ; i < 9 ; i++){
+            if(arr[i] != arr[i + 1]){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        sc.close();
+    }
+}
