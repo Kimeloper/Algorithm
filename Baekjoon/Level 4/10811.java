d78c7560b3d71b7f238293a7283333414c7ea665
Kimeloper <sesame.kim321@gmail.com>
2024-12-13 18:53:06 +0900

    Create 10811 바구니 뒤집기.java
    [Bronze II]
    
    배열을 뒤집는 문제
    https://www.acmicpc.net/problem/10811

---
+++ Baekjoon/Level 4/10811.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        int M = sc.nextInt();
+        int temp;
+
+        for(int a = 0 ; a < N ; a++){
+            arr[a] = a + 1;
+        }
+
+        for(int a = 0 ; a < M ; a++){
+            int i = sc.nextInt()-1;
+            int j = sc.nextInt()-1;
+
+            while(i < j){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        for(int a = 0 ; a < N ; a++){
+            System.out.print(arr[a] + " ");
+        }
+
+        sc.close();
+    }
+}
