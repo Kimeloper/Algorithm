2ba76ad0b1df64cf2fef2ede192d25ddae357c2e
Kimeloper <sesame.kim321@gmail.com>
2025-02-05 23:58:06 +0900

    Create 2231 분해합.java
    [Bronze II]
    
    모든 경우를 시도하여 N의 생성자를 구하는 문제
    https://www.acmicpc.net/problem/2231

---
+++ Baekjoon/Level 12/2231.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int result = 0 ;
+
+        for(int i = 0 ; i < N ;i++){
+            int num = i;
+            int sum = i;
+
+            while(num != 0){
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if(sum == N){
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+      
+        sc.close();
+    }
+
+}
