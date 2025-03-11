75ecd4e56baa7000a1ef3f46111cd9940a2d3771
Kimeloper <sesame.kim321@gmail.com>
2025-03-11 23:54:55 +0900

    Create 1934 최소공배수.java
    [Bronze I]
    
    최대공약수만 구해도 최소공배수를 바로 알 수 있습니다.
    https://www.acmicpc.net/problem/1934

---
+++ Baekjoon/Level 14/1934.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 0 ; i < T ; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            System.out.println((A * B) / gcd(A, B));
+        }
+    }
+
+    public static int gcd(int A , int B){
+        if (B == 0) {
+            return A;
+        }else{
+            return gcd(B, A % B);
+        }
+    }
+}
