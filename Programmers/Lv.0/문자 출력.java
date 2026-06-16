5b66991003b68bf191caf4b2bc4efd261beba7a3
Kimeloper <186737319+Kimeloper@users.noreply.github.com>
2026-06-16 16:59:42 +0900

    [PCCE 기출문제] 1번 / 문자 출력
    https://school.programmers.co.kr/learn/courses/30/lessons/340207

---
+++ Programmers/Lv.0/문자 출력.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
+
+/* 
+  "\n"은 개행 문자로, 줄바꿈 할 때 사용한다.
+*/
