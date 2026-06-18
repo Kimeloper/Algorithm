10f83633c4c810826962e9368eaf4767dd0afcf4
Kimeloper <186737319+Kimeloper@users.noreply.github.com>
2026-06-18 23:48:42 +0900

    특수문자 출력하기
    https://school.programmers.co.kr/learn/courses/30/lessons/181948

---
+++ Programmers/Lv.0/특수문자 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
+
+/*
+ 큰따옴표(")와 역슬래시(\)는 그대로 쓰면 오류가 발생한다.
+ 따라서 이스케이프 문자를 사용해야한다.
+
+ 큰따옴표(") => \"
+ 역슬래시(\) => \\
+*/
