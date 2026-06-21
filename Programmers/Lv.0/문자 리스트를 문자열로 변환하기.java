51647b22ca1a72ff6019dae27377af94dae85540
Kimeloper <186737319+Kimeloper@users.noreply.github.com>
2026-06-21 23:59:38 +0900

    문자 리스트를 문자열로 변환하기
    https://school.programmers.co.kr/learn/courses/30/lessons/181941

---
+++ Programmers/Lv.0/문자 리스트를 문자열로 변환하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        
+        for(int i = 0 ; i < arr.length; i++){
+            String str = arr[i];
+            answer += str;
+        }
+        return answer;
+    }
+}
